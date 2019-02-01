02f1979a363a8c3bdf5b889031ec0444a2b7fe48
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-01 15:47:23 +0000

    Deploy AhmadKabakibi/foursquare-venues to github.com/AhmadKabakibi/foursquare-venues.git:gh-pages

---
+++ precache-manifest.f4c7c5c077653e8a1162c6d67e0e2a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4062d4e1ebb93dfc5f14",
+    "url": "/foursquare-venues/static/css/main.33420be2.chunk.css"
+  },
+  {
+    "revision": "4062d4e1ebb93dfc5f14",
+    "url": "/foursquare-venues/static/js/main.4062d4e1.chunk.js"
+  },
+  {
+    "revision": "7586061e760bbadd52d9",
+    "url": "/foursquare-venues/static/css/1.55e026b9.chunk.css"
+  },
+  {
+    "revision": "7586061e760bbadd52d9",
+    "url": "/foursquare-venues/static/js/1.7586061e.chunk.js"
+  },
+  {
+    "revision": "bedce8cbbf2aea5b8f12",
+    "url": "/foursquare-venues/static/js/runtime~main.bedce8cb.js"
+  },
+  {
+    "revision": "997e6ec1560f7540089a1af1823ba1aa",
+    "url": "/foursquare-venues/index.html"
+  }
+];
